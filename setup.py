90af9b8829b9c6f71e3271eebdc826b7bb7bf61e
Arian <dev.arian@gmail.com>
2023-11-06 16:47:14 +0330

    added setup py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'This is a lightweight library that works as a connector to Bitget pulic API'
+LONG_DESCRIPTION = 'SDK for bitget exchange supporting rest and websocket API.'
+
+setup(
+    name="bitget-connector",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Arian Bahrampour",
+    author_email="dev.arianbahrampour1380@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'bitget', 'sdk', 'api', 'wrapper'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
